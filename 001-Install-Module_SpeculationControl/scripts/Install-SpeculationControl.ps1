71d83dac8ca16943ed19074a45110a9c0bf4e2fe
marc4018 <marcus.canales@RACKSPACE.COM>
2018-01-10 13:09:08 -0600

    Added NuGet Install

--- 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
+++ 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
@@ -1,2 +1,3 @@
-Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force -Confirm:$False
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
+Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force -Confirm:$False | Out-Null
 Get-SpeculationControlSettings
