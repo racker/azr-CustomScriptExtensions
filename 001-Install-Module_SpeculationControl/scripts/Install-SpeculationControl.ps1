d8bb0470721f6814181966172acac00da43933cb
marc4018 <marcus.canales@RACKSPACE.COM>
2018-01-10 13:01:35 -0600

    Added -Confirm:$False

--- 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
+++ 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
@@ -1,2 +1,2 @@
-Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force
+Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force -Confirm:$False
 Get-SpeculationControlSettings
