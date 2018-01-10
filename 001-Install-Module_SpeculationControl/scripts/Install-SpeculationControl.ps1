ee7ff2926382005b206fc9f15b4420e21040d903
marc4018 <marcus.canales@RACKSPACE.COM>
2018-01-10 15:53:20 -0600

    Added download and install for the package manager

--- 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
+++ 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
@@ -1,3 +1,8 @@
+$url = 'https://github.com/racker/azr-CustomScriptExtensions/raw/master/PackageManagement_x64.msi'
+$output = "D:\PackageManagement_x64.msi"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+Start-Process msiexec.exe -Wait -ArgumentList '/I "D:\PackageManagement_x64.msi" /quiet'
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
 Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force -Confirm:$False | Out-Null
 Get-SpeculationControlSettings
