a57a087da38f836e7006d4b2b2f7ead79a31ab04
marc4018 <marcus.canales@RACKSPACE.COM>
2018-01-15 13:11:43 -0600

    Added Line to restart Windows VM Agent Service

--- 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
+++ 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
@@ -5,4 +5,5 @@ $wc.DownloadFile($url, $output)
 Start-Process msiexec.exe -Wait -ArgumentList '/I "D:\PackageManagement_x64.msi" /quiet'
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
 Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force -Confirm:$False | Out-Null
+Restart-Service -Name 'WindowsAzureGuestAgent' -Force | Out-Null
 Get-SpeculationControlSettings
