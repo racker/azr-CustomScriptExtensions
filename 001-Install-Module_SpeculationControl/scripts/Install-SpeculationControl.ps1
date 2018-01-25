3dd79e1b72d046e304d0d982f7c6326198b76ff2
marc4018 <marcus.canales@RACKSPACE.COM>
2018-01-25 14:56:54 -0600

    Added soruce for nuget

--- 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
+++ 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
@@ -3,7 +3,7 @@ $output = "D:\PackageManagement_x64.msi"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
 Start-Process msiexec.exe -Wait -ArgumentList '/I "D:\PackageManagement_x64.msi" /quiet'
-Register-PSRepository -Name "PSGallery" –SourceLocation "https://www.powershellgallery.com/api/v2/" -InstallationPolicy Trusted
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
+Register-PSRepository -Name "PSGallery" –SourceLocation "https://www.powershellgallery.com/api/v2/" -InstallationPolicy Trusted | Out-Null
+Install-PackageProvider -Name NuGet -Source "https://www.nuget.org/api/v2/" -Force | Out-Null
 Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force -Confirm:$False | Out-Null
 Get-SpeculationControlSettings
