908e325913f6ec994d516b1d8bf704dbef20c6cf
Marcus Canales <magno101@gmail.com>
2018-01-30 08:32:03 -0600

    commented out register and install lines due to errors on VMs.

--- 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
+++ 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
@@ -3,7 +3,7 @@ $output = "D:\PackageManagement_x64.msi"
 $wc = New-Object System.Net.WebClient -ErrorAction SilentlyContinue
 $wc.DownloadFile($url, $output)
 Start-Process msiexec.exe -Wait -ArgumentList '/I "D:\PackageManagement_x64.msi" /quiet' -ErrorAction SilentlyContinue
-Register-PSRepository -Name "PSGallery" –SourceLocation "https://www.powershellgallery.com/api/v2/" -InstallationPolicy Trusted -ErrorAction SilentlyContinue | Out-Null
-Install-PackageProvider -Name NuGet -Source "https://www.nuget.org/api/v2/" -Force -ErrorAction SilentlyContinue | Out-Null
+# Register-PSRepository -Name "PSGallery" –SourceLocation "https://www.powershellgallery.com/api/v2/" -InstallationPolicy Trusted -ErrorAction SilentlyContinue | Out-Null
+# Install-PackageProvider -Name NuGet -Source "https://www.nuget.org/api/v2/" -Force -ErrorAction SilentlyContinue | Out-Null
 Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force -Confirm:$False -ErrorAction SilentlyContinue | Out-Null
 Get-SpeculationControlSettings
