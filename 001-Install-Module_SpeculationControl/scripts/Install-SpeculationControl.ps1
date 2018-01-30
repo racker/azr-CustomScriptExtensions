821f1ca38d7d8d4be986b7fd30191759b4f56389
Marcus Canales <magno101@gmail.com>
2018-01-30 00:26:16 -0600

    Error Action Silent

--- 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
+++ 001-Install-Module_SpeculationControl/scripts/Install-SpeculationControl.ps1
@@ -1,9 +1,9 @@
 $url = 'https://github.com/racker/azr-CustomScriptExtensions/raw/master/PackageManagement_x64.msi'
 $output = "D:\PackageManagement_x64.msi"
-$wc = New-Object System.Net.WebClient
+$wc = New-Object System.Net.WebClient -ErrorAction SilentlyContinue
 $wc.DownloadFile($url, $output)
-Start-Process msiexec.exe -Wait -ArgumentList '/I "D:\PackageManagement_x64.msi" /quiet'
-Register-PSRepository -Name "PSGallery" –SourceLocation "https://www.powershellgallery.com/api/v2/" -InstallationPolicy Trusted | Out-Null
-Install-PackageProvider -Name NuGet -Source "https://www.nuget.org/api/v2/" -Force | Out-Null
-Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force -Confirm:$False | Out-Null
+Start-Process msiexec.exe -Wait -ArgumentList '/I "D:\PackageManagement_x64.msi" /quiet' -ErrorAction SilentlyContinue
+Register-PSRepository -Name "PSGallery" –SourceLocation "https://www.powershellgallery.com/api/v2/" -InstallationPolicy Trusted -ErrorAction SilentlyContinue | Out-Null
+Install-PackageProvider -Name NuGet -Source "https://www.nuget.org/api/v2/" -Force -ErrorAction SilentlyContinue | Out-Null
+Find-Module -name SpeculationControl | Install-Module -Scope AllUsers -Force -Confirm:$False -ErrorAction SilentlyContinue | Out-Null
 Get-SpeculationControlSettings
