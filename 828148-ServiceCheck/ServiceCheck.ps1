cfc49070426c359bfd7ebf921c2ecb9dc0445592
Marcus Canales <magno101@gmail.com>
2018-02-04 21:01:27 -0600

    Modified to allow for multiple Services to be submitted.

--- 828148-ServiceCheck/ServiceCheck.ps1
+++ 828148-ServiceCheck/ServiceCheck.ps1
@@ -1,19 +1,21 @@
 ﻿[cmdletbinding()]
 param(
-  [string]$ServiceName,
+  [string[]]$ServiceName,
   [string]$vmid
 )
 
 Try {
-  $Svc = Get-Service -Name $ServiceName -ErrorAction Stop
+  $Svc = Get-Service -Name $ServiceName.split(',').trim() -ErrorAction Stop
   #Format OMS Report Object
-  $Message = New-Object PSObject ([ordered]@{
-    Computer=$env:COMPUTERNAME
-    SvcDisplay=$Svc.DisplayName
-    SvcName=$Svc.Name
-    SvcState=($Svc.Status).tostring()
-    SvcStartType = ($Svc.StartType).ToString()
-    ResourceId = $vmid
+  $Message = @(ForEach ($Service in $Svc) {
+    New-Object PSObject ([ordered]@{
+      Computer=$env:COMPUTERNAME
+      SvcDisplay=$Svc.DisplayName
+      SvcName=$Svc.Name
+      SvcState=($Svc.Status).tostring()
+      SvcStartType = ($Svc.StartType).ToString()
+      ResourceId = $vmid
+    })
   })
   $MessageJSONString = [string]($Message | ConvertTo-JSON) 
   Write-Output -InputObject $MessageJSONString
@@ -21,13 +23,15 @@ Try {
 }
 Catch [Microsoft.PowerShell.Commands.ServiceCommandException] {
   Write-Error -Exception $_.Exception -Message "$($_.TargetObject) Service Not Found"
-  $Message = New-Object PSObject ([ordered]@{
-    Computer = $env:COMPUTERNAME
-    SvcDisplay = "Service Not Found"
-    SvcName = $ServiceName
-    SvcState = "Service Not Found"
-    SvcStartType = "Service Not Found"
-    ResourceId = $vmid
+  $Message = @(ForEach ($Service in $Svc) {
+    New-Object PSObject ([ordered]@{
+      Computer=$env:COMPUTERNAME
+      SvcDisplay=$Svc.DisplayName
+      SvcName=$Svc.Name
+      SvcState=($Svc.Status).tostring()
+      SvcStartType = ($Svc.StartType).ToString()
+      ResourceId = $vmid
+    })
   })
   $MessageJSONString = [string]($Message | ConvertTo-JSON) 
   Write-Output -InputObject $MessageJSONString
