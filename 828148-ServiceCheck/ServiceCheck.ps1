52baf21ba346d2997b5198b59d824b5525080ccf
Marcus Canales <magno101@gmail.com>
2018-02-05 19:17:32 -0600

    Rewrite to take both Services and Processes

--- 828148-ServiceCheck/ServiceCheck.ps1
+++ 828148-ServiceCheck/ServiceCheck.ps1
@@ -1,42 +1,69 @@
 ﻿[cmdletbinding()]
 param(
   [string[]]$ServiceName,
+  [string[]]$ProcessName,
   [string]$vmid
 )
+$Message = @()
 
-Try {
-  $Svc = Get-Service -Name $ServiceName.split(',').trim() -ErrorAction Stop
-  #Format OMS Report Object
-  $Message = @(ForEach ($Service in $Svc) {
-    $Report = New-Object PSObject -Property ([ordered]@{
-      Computer=$env:COMPUTERNAME
-      SvcDisplay=$Service.DisplayName
-      SvcName=$Service.Name
-      SvcState=($Service.Status).tostring()
-      SvcStartType = ($Service.StartType).tostring()
-      ResourceId = $vmid
-    })
-    $Report
-  })
-  $MessageJSONString = [string]($Message | ConvertTo-JSON) 
-  Write-Output -InputObject $MessageJSONString
-  Remove-Variable -Name MessageJSONString,vmid,svc,Message,ServiceName,Report -Force -ErrorAction SilentlyContinue | Out-Null
+if ($ServiceName) {
+  ForEach ( $Service in $ServiceName.split(',').trim() ){
+    Try {
+      $Svc = Get-Service -Name $Service -ErrorAction Stop
+      #Format OMS Report Object
+      $Message += New-Object PSObject -Property ([ordered]@{
+            Computer     = $env:COMPUTERNAME
+            SvcDisplay   = $Svc.DisplayName
+            SvcName      = $Svc.Name
+            SvcState     = ($Svc.Status).tostring()
+            SvcStartType = ($Svc.StartType).tostring()
+            ResourceId   = $vmid
+          })
+    }
+    Catch [Microsoft.PowerShell.Commands.ServiceCommandException]{
+      Write-Error -Exception $_.Exception -Message "$($_.TargetObject) Service Not Found"
+      #Format OMS Report Object
+      $Message += New-Object PSObject ([ordered]@{
+            Computer     = $env:COMPUTERNAME
+            SvcDisplay   = "Service Not Found"
+            SvcName      = $Service 
+            SvcState     = "Service Not Found"
+            SvcStartType = "Service Not Found"
+            ResourceId   = $vmid
+          })
+      Continue
+    }
+  }
+  Remove-Variable -Name Svc -ErrorAction SilentlyContinue -Force | Out-Null
 }
-Catch [Microsoft.PowerShell.Commands.ServiceCommandException] {
-  Write-Error -Exception $_.Exception -Message "$($_.TargetObject) Service Not Found"
-  $Message = @(ForEach ($Service in $Svc) {
-    New-Object PSObject ([ordered]@{
-      Computer=$env:COMPUTERNAME
-      SvcDisplay=$Svc.DisplayName
-      SvcName=$Svc.Name
-      SvcState=($Svc.Status).tostring()
-      SvcStartType = ($Svc.StartType).ToString()
-      ResourceId = $vmid
-    })
-  })
-  $MessageJSONString = [string]($Message | ConvertTo-JSON) 
-  Write-Output -InputObject $MessageJSONString
-  Remove-Variable -Name MessageJSONString,vmid,Message,ServiceName -Force -ErrorAction SilentlyContinue | Out-Null
+if ($ProcessName) {
+  ForEach ( $Process in $ProcessName.split(',').trim() ) {
+    Try {
+      $Proc = Get-Process -Name $Process -ErrorAction Stop
+      #Format OMS Report Object
+      $Message += New-Object PSObject -Property ([ordered]@{
+            Computer   = $env:COMPUTERNAME
+            ProcName   = $Proc.Name
+            ProcPath   = $Proc.Path
+            ProcState  = $Proc.Responding
+            ResourceId = $vmid
+          })
+    }
+    Catch [Microsoft.PowerShell.Commands.ProcessCommandException] {
+      Write-Error -Exception $_.Exception -Message "$($_.TargetObject) Process Not Found"
+      $Message += New-Object PSObject ([ordered]@{
+            Computer   = $env:COMPUTERNAME
+            ProcName   = $Process
+            ProcPath   = "Process Not Found"
+            ProcState  = "Stopped"
+            ResourceId = $vmid
+          })
+      Continue
+    }
+  }
+  Remove-Variable -Name Proc -ErrorAction SilentlyContinue -Force | Out-Null
 }
-
-
+# Convert the $Message Object to JSON for better parsing when received by the Invoke-AzureRMVMRunCommand
+$MessageJSONString = [string]($Message | ConvertTo-JSON) 
+Write-Output -InputObject $MessageJSONString
+Remove-Variable -Name MessageJSONString,Message,vmid,ProcessName,ServiceName -Force -ErrorAction SilentlyContinue | Out-Null
