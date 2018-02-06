aa1eedd36541d65818b8bfdd5a831b68a56614b9
Marcus Canales <magno101@gmail.com>
2018-02-05 19:19:08 -0600

    Formatting Change

--- 828148-ServiceCheck/ServiceCheck.ps1
+++ 828148-ServiceCheck/ServiceCheck.ps1
@@ -7,30 +7,30 @@ param(
 $Message = @()
 
 if ($ServiceName) {
-  ForEach ( $Service in $ServiceName.split(',').trim() ){
+  ForEach ( $Service in $ServiceName.split(',').trim() ) {
     Try {
       $Svc = Get-Service -Name $Service -ErrorAction Stop
       #Format OMS Report Object
       $Message += New-Object PSObject -Property ([ordered]@{
-            Computer     = $env:COMPUTERNAME
-            SvcDisplay   = $Svc.DisplayName
-            SvcName      = $Svc.Name
-            SvcState     = ($Svc.Status).tostring()
-            SvcStartType = ($Svc.StartType).tostring()
-            ResourceId   = $vmid
-          })
+          Computer     = $env:COMPUTERNAME
+          SvcDisplay   = $Svc.DisplayName
+          SvcName      = $Svc.Name
+          SvcState     = ($Svc.Status).tostring()
+          SvcStartType = ($Svc.StartType).tostring()
+          ResourceId   = $vmid
+        })
     }
-    Catch [Microsoft.PowerShell.Commands.ServiceCommandException]{
+    Catch [Microsoft.PowerShell.Commands.ServiceCommandException] {
       Write-Error -Exception $_.Exception -Message "$($_.TargetObject) Service Not Found"
       #Format OMS Report Object
       $Message += New-Object PSObject ([ordered]@{
-            Computer     = $env:COMPUTERNAME
-            SvcDisplay   = "Service Not Found"
-            SvcName      = $Service 
-            SvcState     = "Service Not Found"
-            SvcStartType = "Service Not Found"
-            ResourceId   = $vmid
-          })
+          Computer     = $env:COMPUTERNAME
+          SvcDisplay   = "Service Not Found"
+          SvcName      = $Service 
+          SvcState     = "Service Not Found"
+          SvcStartType = "Service Not Found"
+          ResourceId   = $vmid
+        })
       Continue
     }
   }
@@ -42,22 +42,22 @@ if ($ProcessName) {
       $Proc = Get-Process -Name $Process -ErrorAction Stop
       #Format OMS Report Object
       $Message += New-Object PSObject -Property ([ordered]@{
-            Computer   = $env:COMPUTERNAME
-            ProcName   = $Proc.Name
-            ProcPath   = $Proc.Path
-            ProcState  = $Proc.Responding
-            ResourceId = $vmid
-          })
+          Computer   = $env:COMPUTERNAME
+          ProcName   = $Proc.Name
+          ProcPath   = $Proc.Path
+          ProcState  = $Proc.Responding
+          ResourceId = $vmid
+        })
     }
     Catch [Microsoft.PowerShell.Commands.ProcessCommandException] {
       Write-Error -Exception $_.Exception -Message "$($_.TargetObject) Process Not Found"
       $Message += New-Object PSObject ([ordered]@{
-            Computer   = $env:COMPUTERNAME
-            ProcName   = $Process
-            ProcPath   = "Process Not Found"
-            ProcState  = "Stopped"
-            ResourceId = $vmid
-          })
+          Computer   = $env:COMPUTERNAME
+          ProcName   = $Process
+          ProcPath   = "Process Not Found"
+          ProcState  = "Stopped"
+          ResourceId = $vmid
+        })
       Continue
     }
   }
@@ -66,4 +66,4 @@ if ($ProcessName) {
 # Convert the $Message Object to JSON for better parsing when received by the Invoke-AzureRMVMRunCommand
 $MessageJSONString = [string]($Message | ConvertTo-JSON) 
 Write-Output -InputObject $MessageJSONString
-Remove-Variable -Name MessageJSONString,Message,vmid,ProcessName,ServiceName -Force -ErrorAction SilentlyContinue | Out-Null
+Remove-Variable -Name MessageJSONString, Message, vmid, ProcessName, ServiceName -Force -ErrorAction SilentlyContinue | Out-Null
